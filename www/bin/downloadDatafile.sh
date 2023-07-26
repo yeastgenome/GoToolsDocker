7fe4a3f4aad90d395a766ab686f47ee7828363a6
stuartmiyasato <stuartmiyasato@users.noreply.github.com>
2023-07-26 15:35:21 -0700

    removed the IEA and other computational annotations from slim terms generation

--- www/bin/downloadDatafile.sh
+++ www/bin/downloadDatafile.sh
@@ -32,20 +32,25 @@ fi
 
 /bin/cp -p ../gene_association.sgd ../gene_association.sgd_old
 /bin/cp -p ../gene_ontology.obo ../gene_ontology.obo_old
-/bin/mv sgd.gaf ../gene_association.sgd
+
+/usr/bin/grep -v "$(printf '\t')IBA$(printf '\t')" sgd.gaf | /usr/bin/grep -v "$(printf '\t')IEA$(printf '\t')" | /usr/bin/grep -v "$(printf '\t')CPX-" > ../gene_association_mapper.sgd 
+/usr/bin/grep -v -v "$(printf '\t')CPX-" sgd.gaf > ../gene_association.sgd
+
 /bin/mv go-basic.obo ../gene_ontology.obo
+/bin/cp ../gene_association.sgd ../unparsed/
+/bin/cp ../gene_ontology.obo ../unparsed/
 
 echo "creating slim component gaf file..." | /bin/tee -a $OUTPUT_FILE
 
-/var/www/bin/map2slim /var/www/data/slim_component.lst /var/www/data/gene_ontology.obo /var/www/data/gene_association.sgd -o /var/www/data/new/slim_component_gene_association.sgd 2>&1 | /bin/tee -a $OUTPUT_FILE
+/var/www/bin/map2slim /var/www/data/slim_component.lst /var/www/data/gene_ontology.obo /var/www/data/gene_association_mapper.sgd -o /var/www/data/new/slim_component_gene_association.sgd 2>&1 | /bin/tee -a $OUTPUT_FILE
 
 echo "creating slim process gaf file..." | /bin/tee -a $OUTPUT_FILE
 
-/var/www/bin/map2slim /var/www/data/slim_process.lst /var/www/data/gene_ontology.obo /var/www/data/gene_association.sgd -o /var/www/data/new/slim_process_gene_association.sgd 2>&1 | /bin/tee -a $OUTPUT_FILE
+/var/www/bin/map2slim /var/www/data/slim_process.lst /var/www/data/gene_ontology.obo /var/www/data/gene_association_mapper.sgd -o /var/www/data/new/slim_process_gene_association.sgd 2>&1 | /bin/tee -a $OUTPUT_FILE
 
 echo "creating slim function gaf file..." | /bin/tee -a $OUTPUT_FILE
 
-/var/www/bin/map2slim /var/www/data/slim_function.lst /var/www/data/gene_ontology.obo /var/www/data/gene_association.sgd -o /var/www/data/new/slim_function_gene_association.sgd 2>&1 | /bin/tee -a $OUTPUT_FILE
+/var/www/bin/map2slim /var/www/data/slim_function.lst /var/www/data/gene_ontology.obo /var/www/data/gene_association_mapper.sgd -o /var/www/data/new/slim_function_gene_association.sgd 2>&1 | /bin/tee -a $OUTPUT_FILE
 
 /bin/cp -p ../slim_component_gene_association.sgd ../slim_component_gene_association.sgd_old
 /bin/cp -p ../slim_process_gene_association.sgd ../slim_process_gene_association.sgd_old
